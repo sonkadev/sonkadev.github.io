5dc82ba06cb003cc9b0e98605970cd4ab59fad79
sonka <me@sonka.lol>
2025-03-05 11:39:16 +0100

    added bg, modified particles, and made the site somewhat responsive

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -6,5 +6,11 @@ function isMobileDevice() {
 
 if (isMobileDevice()) {
   console.log("a kurva anyád");
-  alert("this site only works properly on pcs")
+  alert("this site only works properly on pcs");
+  document.body.style.display = "flex";
+  document.body.style.flexDirection = "row";
+  const div = document.getElementById("box1");
+  div.style.width = "75vw";
+  div.style.height = "20vh";
+  document.body.style.fontSize = "5vw";
 }
